0adb6f512fba1699a010e9e397b92062e92c9430
eliot <eliot@eliotz.dev>
2022-12-15 22:40:40 -0600

    :)

--- ws-server/chat/chatroom/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChatroomConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chatroom'
-
-    def ready(self):
-        import chatroom.signals
--- ws-server/scratch/wsgi.py
+++
@@ -1,3 +0,0 @@
-from mp_practice import app
-if __name__ == '__main__':
-    app.run()
